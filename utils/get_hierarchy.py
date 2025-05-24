c2a29f3ecef3e9619cc084fb01e0b79086e7e08f
EMLSLab <surafel29693@gmail.com>
2025-05-24 13:40:20 +0300

    Merge Util

--- utils/get_hierarchy.py
+++ utils/get_hierarchy.py
@@ -63,14 +63,14 @@ def get_child_organization(session: SessionDep, organization_id: int , max_depth
     
     return {
             'id': organization_id,
-            "organization": org.organization_name,
+            'name': "All" if org.parent_id is None else org.organization_name, 
             "owner": org.owner_name,
             "description": org.description,
             "organization_type": org.organization_type,
             "inheritance_group": org.inheritance_group,
             "parent_organization": parent_org.organization_name,
             "scope_groups": [{"id": sg.id, "scope_name": sg.scope_name} for sg in org.scope_groups],
-            'name': "All" if org.parent_id is None else org.organization_name, 
+
             children_key: [get_child_organization(session, child.id, max_depth-1 if max_depth is not None else max_depth, children_key) for child in children if (max_depth is None or max_depth > 0)]           
         }
     
